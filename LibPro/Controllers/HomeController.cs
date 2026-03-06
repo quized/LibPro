bf9a85c1a8e71c2a8e4eafd0a72bab715d2638f4
quized22 <quized22@gmail.com>
2026-03-06 09:22:25 +0800

    3/6(VC初建)

--- LibPro/Controllers/HomeController.cs
+++ LibPro/Controllers/HomeController.cs
@@ -34,6 +34,7 @@ namespace LibPro.Controllers
             return View();
         }
 
+        
       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ LibPro/ViewComponents/VCAnn.cs
@@ -0,0 +1,22 @@
+﻿using LibPro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibPro.ViewComponents
+{
+    public class VCAnn : ViewComponent
+    {
+        private readonly LibproContext _context;
+        public VCAnn(LibproContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var announcements = _context.Announcements
+                .OrderByDescending(a => a.CreatedDate)
+                .Take(10) 
+                .ToList();
+            return View(announcements);
+        }
+    }
+}
