b7452ef986504e9f9ac8845ebe5b3cf0a3d1fc6a
quized22 <quized22@gmail.com>
2026-03-17 11:59:25 +0800

    3/17

--- LibPro/Controllers/UserAccountsController.cs
+++ LibPro/Controllers/UserAccountsController.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using LibPro.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using LibPro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace LibPro.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class UserAccountsController : Controller
     {
         private readonly LibproContext _context;
