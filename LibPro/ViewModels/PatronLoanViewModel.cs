54da11d5cafb4600f57e124fff62904f1b11494b
quized22 <quized22@gmail.com>
2026-03-19 16:31:56 +0800

    3/19

---
+++ LibPro/ViewModels/PatronLoanViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace LibPro.ViewModels
+{
+    public class PatronLoanViewModel
+    {
+        public string LoanID { get; set; } = null!;
+        public string BookTitle { get; set; } = null!;
+        public string Author { get; set; } = null!;
+
+        public DateTime LoanDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public byte RenewalCount { get; set; }
+        public bool CanRenew { get; set; }
+        public string RenewMessage { get; set; } = null!;
+        public string ButtonClass { get; set; } = null!;
+    }
+}
